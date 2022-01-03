7e5874927482e44a0c8b0c505542893274d00395
erik margueron <malloryerik@gmail.com>
2022-01-03 15:42:21 +0900

    agent that breaks to show supervisor

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -3,6 +3,8 @@ defmodule Dictionary.Runtime.Server do
 
   @me __MODULE__
 
+  use Agent
+
   alias Dictionary.Impl.WordList
 
   # iex> {:ok, pid} = Dictionary.Runtime.Server.start_link
@@ -11,11 +13,15 @@ defmodule Dictionary.Runtime.Server do
   # iex> Dictionary.Runtime.Server.random_word(pid)
   # "code"
 
-  def start_link() do
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
