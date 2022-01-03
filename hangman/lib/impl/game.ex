926f7c08c2be2f7bbe60c7c23fa0b5a9af92f4ff
erik margueron <malloryerik@gmail.com>
2022-01-03 18:29:42 +0900

    fix Hangman.Game.new_game

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -19,7 +19,7 @@ defmodule Hangman.Impl.Game do
 
   @spec new_game() :: t
   def new_game do
-    new_game(Dictionary.random_word(Dictionary.start()))
+    new_game(Dictionary.random_word())
   end
 
   @spec new_game(String.t()) :: t
--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -9,7 +9,12 @@ defmodule Hangman.Runtime.Server do
   end
 
   ### Server process code
+
+  # called back in new process
+  # receives as param what was passed in at start_link (nil here)
+  #                                      -- we ignore this time.
   def init(_) do
+    # {:ok, state} -- here we start a new game and keep as state in genserver
     {:ok, Game.new_game()}
   end
 end
